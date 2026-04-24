73bcf32d85bf587a50fccd294f753c5b33dc982c
Andrewwango <andrew.wang27@gmail.com>
2026-04-24 11:11:52 +0100

    add pdhg

--- mri_recon/reconstruction/classic.py
+++ mri_recon/reconstruction/classic.py
@@ -101,3 +101,34 @@ class TVFISTAReconstructor(dinv.models.Reconstructor):
             dinv.utils.plotting.plot_curves(metrics)
 
         return out
+
+class TVPDHGReconstructor(dinv.models.Reconstructor):
+    """
+    PDHG (Primal Dual Hybrid Gradient a.k.a Chambolle-Pock) algorithm regularised by Total Variation (TV) regulariser.
+
+    :param int n_iter: max number of iterations to run FISTA. Defaults to 100.
+    :param bool verbose: whether plot converge plots
+    """
+    def __init__(self, n_iter: int = 100, verbose: bool = True, **kwargs):
+        super().__init__()
+
+        self.model = dinv.optim.PDCP(
+            prior=dinv.optim.prior.TVPrior(n_it_max=100),
+            data_fidelity=dinv.optim.data_fidelity.L2(),
+            stepsize=0.2, # tau
+            stepsize_dual=0.2, # sigma
+            lambda_reg=2e-4,  # TV regularisation parameter,
+            early_stop=True,
+            max_iter=n_iter,
+            verbose=verbose,
+            # thres_conv=1e-7,
+        )
+
+    def forward(self, y, physics):
+        out = self.model(y, physics, compute_metrics=self.model.verbose)
+        
+        if self.model.verbose:
+            out, metrics = out
+            dinv.utils.plotting.plot_curves(metrics)
+
+        return out
